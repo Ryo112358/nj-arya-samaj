3d8b3b132c40a66501db4656f6e70da28d1d48ca
Ryo112358 <ryo112358@live.com>
2019-10-23 22:00:08 -0400

    Completed PayPal donation story. Button press redirects to PayPal page in new tab.

--- src/app/shared/layout/donate-via-paypal/donate-via-paypal.component.ts
+++ src/app/shared/layout/donate-via-paypal/donate-via-paypal.component.ts
@@ -7,56 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DonateViaPaypalComponent implements OnInit {
 
-  constructor() { }
+  paypalDonationURL: String;
+
+  constructor() {
+    this.paypalDonationURL = "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=89L2EXPC4UBMC&source=url";
+  }
 
   ngOnInit() {
-  }
-
-  addScript: boolean = false;
-  paypalLoad: boolean = true;
-  
-  finalAmount: number = 1;
- 
-  paypalConfig = {
-    env: 'sandbox',
-    client: {
-      sandbox: 'sb',
-      production: 'sb'
-    },
-    commit: true,
-    payment: (data, actions) => {
-      return actions.payment.create({
-        payment: {
-          transactions: [
-            { amount: { total: this.finalAmount, currency: 'USD' } }
-          ]
-        }
-      });
-    },
-    onAuthorize: (data, actions) => {
-      return actions.payment.execute().then((payment) => {
-        //Do something when payment is successful.
-      })
-    }
-  };
-  
-  addPaypalScript() {
-    this.addScript = true;
-    return new Promise((resolve, reject) => {
-      let scriptTagElement = document.createElement('script');    
-      scriptTagElement.src = 'https://www.paypalobjects.com/api/checkout.js';
-      scriptTagElement.onload = resolve;
-      document.body.appendChild(scriptTagElement);
-    })
-  }
-
-  ngAfterViewChecked(): void {
-    if (!this.addScript) {
-      this.addPaypalScript().then(() => {
-        paypal.Button.render(this.paypalConfig, '#paypal-button-container');
-        this.paypalLoad = false;
-      })
-    }
   }
 
 }
